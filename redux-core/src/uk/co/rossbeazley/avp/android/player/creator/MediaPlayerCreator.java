b070c21bcf9f84ac1ef6f887ddf8fad502b65b52
rossbeazley <ross@rossbeazley.co.uk>
2013-10-17 22:19:01 +0100

    some rework notes added

--- redux-core/src/uk/co/rossbeazley/avp/android/player/creator/MediaPlayerCreator.java
+++ redux-core/src/uk/co/rossbeazley/avp/android/player/creator/MediaPlayerCreator.java
@@ -3,6 +3,7 @@ package uk.co.rossbeazley.avp.android.player.creator;
 import uk.co.rossbeazley.avp.UriString;
 import uk.co.rossbeazley.avp.android.mediaplayer.MediaPlayer;
 
+//I could split this interface into two, creation and announcement, the tests would look neater
 public interface MediaPlayerCreator {
     void create(UriString any_uri_string);
 
