2999b5e7933809eeb8bb240f1218bce6a5e913ce
rossbeazley <ross@rossbeazley.co.uk>
2013-11-01 10:41:54 +0000

    failing test for the state watcher dispatcher thingy

--- avp-core/test/unit/src/uk/co/rossbeazley/avp/android/player/state/MediaPlayerStateEventDispatcherTest.java
+++ avp-core/test/unit/src/uk/co/rossbeazley/avp/android/player/state/MediaPlayerStateEventDispatcherTest.java
@@ -1,13 +1,45 @@
 package uk.co.rossbeazley.avp.android.player.state;
 
 import org.junit.Test;
+import uk.co.rossbeazley.avp.Events;
+import uk.co.rossbeazley.avp.android.player.FakeMediaPlayer;
+import uk.co.rossbeazley.avp.eventbus.EventBus;
+import uk.co.rossbeazley.avp.eventbus.Function;
+import uk.co.rossbeazley.avp.eventbus.executor.ExecutorEventBus;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class MediaPlayerStateEventDispatcherTest {
 
+    private String playerState = "UNKNOWN";
+
     @Test
-    public void whenTransitionsToPausePauseEventRaised() {
-        fail("not speced yet");
+    public void whenTransitionsToPauseFromPlayingPauseEventRaised() {
+
+        FakeMediaPlayer mediaPlayer = FakeMediaPlayer.createStartedFakeMediaPlayer();
+
+        EventBus bus = new ExecutorEventBus();
+        bus.whenEvent(Events.PLAYER_PAUSED)
+                .thenRun(new Function() {
+                    @Override
+                    public void invoke() {
+                        playerState = Events.PLAYER_PAUSED;
+                    }
+                });
+
+
+        new MediaPlayerSateEventDispatcher(mediaPlayer,bus);
+
+        mediaPlayer.pause();
+
+        assertThat(playerState, is(Events.PLAYER_PAUSED));
+
+    }
+
+    private class MediaPlayerSateEventDispatcher {
+        public MediaPlayerSateEventDispatcher(FakeMediaPlayer mediaPlayer, EventBus bus) {
+
+        }
     }
 }
