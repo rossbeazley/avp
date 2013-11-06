a28bd50364fc16bc1c62a463781a7588cc3de4a8
rossbeazley <ross@rossbeazley.co.uk>
2013-11-06 22:11:42 +0000

    first passing test for the scrubber and next failing test

--- avp-core/test/unit/src/uk/co/rossbeazley/avp/android/player/control/MediaPlayerScrubbingTest.java
+++ avp-core/test/unit/src/uk/co/rossbeazley/avp/android/player/control/MediaPlayerScrubbingTest.java
@@ -4,8 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 import uk.co.rossbeazley.avp.Events;
 import uk.co.rossbeazley.avp.TimeInMilliseconds;
+import uk.co.rossbeazley.avp.android.mediaplayer.CanScrubMediaPlayer;
 import uk.co.rossbeazley.avp.android.player.FakeMediaPlayer;
 import uk.co.rossbeazley.avp.eventbus.EventBus;
+import uk.co.rossbeazley.avp.eventbus.FunctionWithParameter;
 import uk.co.rossbeazley.avp.eventbus.executor.ExecutorEventBus;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -21,9 +23,9 @@ public class MediaPlayerScrubbingTest {
     public void setup() {
         mediaPlayer = FakeMediaPlayer.createStartedFakeMediaPlayer();
         bus = new ExecutorEventBus();
+        new MediaPlayerScrubber(bus);
         bus.sendPayload(mediaPlayer)
                 .withEvent(Events.PLAYER_VIDEO_LOADED);
-        new MediaPlayerScrubber(bus);
     }
 
     @Test
@@ -39,7 +41,17 @@ public class MediaPlayerScrubbingTest {
 
     @Test
     public void whenScrubbingNotFinishedUserScrubEventNotProcessed() {
-        fail("not speced yet");
+        TimeInMilliseconds expectedScrubPosition = TimeInMilliseconds.fromLong(5000);
+
+        TimeInMilliseconds secondScrubPosition = TimeInMilliseconds.fromLong(15000);
+
+        bus.sendPayload(expectedScrubPosition)
+                .withEvent(Events.USER_SCRUB);
+
+        bus.sendPayload(secondScrubPosition)
+                .withEvent(Events.USER_SCRUB);
+
+        assertThat(mediaPlayer.seekingTo(),is(expectedScrubPosition));
     }
 
     @Test
@@ -55,8 +67,32 @@ public class MediaPlayerScrubbingTest {
 
 
     private class MediaPlayerScrubber {
-        public MediaPlayerScrubber(EventBus bus) {
 
+        CanScrubMediaPlayer mediaPlayer;
+
+        public MediaPlayerScrubber(EventBus bus) {
+            bindVideoLoadedEvent(bus);
+            bindScrubEvent(bus);
+        }
+
+        private void bindScrubEvent(EventBus bus) {
+            bus.whenEvent(Events.USER_SCRUB)
+                    .thenRun(new FunctionWithParameter<TimeInMilliseconds>() {
+                        @Override
+                        public void invoke(TimeInMilliseconds payload) {
+                            mediaPlayer.seekTo(payload);
+                        }
+                    });
+        }
+
+        private void bindVideoLoadedEvent(EventBus bus) {
+            bus.whenEvent(Events.PLAYER_VIDEO_LOADED)
+                    .thenRun(new FunctionWithParameter<CanScrubMediaPlayer>() {
+                        @Override
+                        public void invoke(CanScrubMediaPlayer payload) {
+                            mediaPlayer = payload;
+                        }
+                    });
         }
     }
 }
