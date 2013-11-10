26629ba723602c35038574c58c3c5b6ab97ffff4
rossbeazley <ross@rossbeazley.co.uk>
2013-11-10 21:47:13 +0000

    pushing some code around, nothing achieved at all

--- avp-droid/src/uk/co/rossbeazley/avp/android/ui/videoplayer/AndroidMediaPlayerVideoOutput.java
+++ avp-droid/src/uk/co/rossbeazley/avp/android/ui/videoplayer/AndroidMediaPlayerVideoOutput.java
@@ -4,44 +4,38 @@ import android.content.Context;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 public class AndroidMediaPlayerVideoOutput implements RenderedVideoOutput {
 
-
-
     @Override
     public void attachToViewGroup(ViewGroup container) {
-
-        final Context context = container.getContext();
-
-        SurfaceView sv = new SurfaceView(context);
-
-        ViewGroup.LayoutParams params = matchParentLayoutParams();
-
-        container.addView(sv,params);
-
-        SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                Toast.makeText(context, "Surface created", Toast.LENGTH_LONG).show();
-                //mediaPlayer.setDisplay(surfaceHolder);
-            }
-
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
-                Toast.makeText(context, "Surface changed", Toast.LENGTH_LONG).show();
-            }
-
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                Toast.makeText(context, "Surface destroyed", Toast.LENGTH_LONG).show();
-            }
-        };
-
-        sv.getHolder().addCallback(callback);
-
+        SurfaceView sv = createSurfaceView(container);
+        addSurfaceViewToViewGroup(sv, container);
     }
 
+    private void addSurfaceViewToViewGroup(SurfaceView sv, ViewGroup container) {
+        ViewGroup.LayoutParams params = matchParentLayoutParams();
+        container.addView(sv, params);
+    }
+
+    private SurfaceView createSurfaceView(ViewGroup container) {
+        final Context context = container.getContext();
+        SurfaceView sv = new SurfaceView(context);
+        sv.getHolder().addCallback(new SurfaceHolderStateChangeCallbacks());
+        return sv;
+    }
 
     private ViewGroup.LayoutParams matchParentLayoutParams() {
         return new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    private static class SurfaceHolderStateChangeCallbacks implements SurfaceHolder.Callback {
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+            //mediaPlayer.setDisplay(surfaceHolder);
+        }
+
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {        }
+
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {        }
     }
 }
