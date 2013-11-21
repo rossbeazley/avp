23b518ab56a4442f4735f4e6d3a7ddb8e19c121c
rossbeazley <ross@rossbeazley.co.uk>
2013-11-21 21:30:26 +0000

    media player view is generated by factory now

--- avp-droid/src/uk/co/rossbeazley/avp/android/player/render/MediaPlayerViewAttachement.java
+++ avp-droid/src/uk/co/rossbeazley/avp/android/player/render/MediaPlayerViewAttachement.java
@@ -1,10 +1,28 @@
 package uk.co.rossbeazley.avp.android.player.render;
 
+import uk.co.rossbeazley.avp.Events;
+import uk.co.rossbeazley.avp.android.mediaplayer.CanAttachToAndroidView;
 import uk.co.rossbeazley.avp.android.ui.videoplayer.CanCreateAndroidMediaPlayerVideoOutput;
+import uk.co.rossbeazley.avp.android.ui.videoplayer.RenderedVideoOutput;
+import uk.co.rossbeazley.avp.eventbus.FunctionWithParameter;
 import uk.co.rossbeazley.avp.eventbus.executor.ExecutorEventBus;
 
 public class MediaPlayerViewAttachement {
-    public MediaPlayerViewAttachement(CanCreateAndroidMediaPlayerVideoOutput canCreateAndroidMediaPlayerVideoOutput, ExecutorEventBus bus) {
+
+
+    public MediaPlayerViewAttachement(final CanCreateAndroidMediaPlayerVideoOutput canCreateAndroidMediaPlayerVideoOutput, final ExecutorEventBus bus) {
+        bus.whenEvent(Events.PLAYER_VIDEO_LOADED)
+                .thenRun(new FunctionWithParameter<CanAttachToAndroidView>() {
+                    @Override
+                    public void invoke(CanAttachToAndroidView payload) {
+                        RenderedVideoOutput mediaPlayerView;
+                        mediaPlayerView = canCreateAndroidMediaPlayerVideoOutput.createAndroidMediaPlayerVideoOutput(payload);
+                        bus .sendPayload(mediaPlayerView)
+                            .withEvent(Events.PLAYER_VIEW_CREATED);
+                    }
+                });
+
+
 
     }
 }
