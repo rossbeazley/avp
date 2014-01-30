74b3413a5f105845f86a1a48be747d4965e3feee
rossbeazley <ross@rossbeazley.co.uk>
2014-01-30 17:10:57 +0000

    moved DI framework into Core module, its droid free

--- avp-core/src/uk/co/rossbeazley/avp/android/application/DependenciesService.java
+++ avp-core/src/uk/co/rossbeazley/avp/android/application/DependenciesService.java
@@ -9,7 +9,7 @@ public class DependenciesService {
 
     void injectDependencies(Object fragment) {
         DependencyInjectors.Injector injector = injectors.injector(fragment.getClass());
-        injector.inject(fragment);    //Use double dispatch to avoid this dodgy code? or DD be more of problem
+        injector.inject(fragment);    //Use double dispatch to avoid this dodgy code? or is DD more of problem, gets everywhere then
     }
 
 }
