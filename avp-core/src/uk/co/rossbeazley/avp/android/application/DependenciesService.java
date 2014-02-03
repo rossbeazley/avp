0a4fec00cce2cdea14d268598cd2ebe84ec050a2
rossbeazley <ross@rossbeazley.co.uk>
2014-02-03 16:07:39 +0000

    tried maintaining levels of abstraction in the function

--- avp-core/src/uk/co/rossbeazley/avp/android/application/DependenciesService.java
+++ avp-core/src/uk/co/rossbeazley/avp/android/application/DependenciesService.java
@@ -9,11 +9,19 @@ public class DependenciesService {
 
     @SuppressWarnings("unchecked")
     void injectDependencies(Object object) {
-        Class<?>[] interfaces = object.getClass().getInterfaces();
-        for (Class interfaceClass : interfaces) {
-            DependencyInjectors.Injector injector = injectors.injector(interfaceClass);
+        Class<?>[] interfaces = findInjectableClasses(object);
+        injectEachClass(object, interfaces);
+    }
+
+    private void injectEachClass(Object object, Class<?>[] injectableClasses) {
+        for (Class injectableClass : injectableClasses) {
+            DependencyInjectors.Injector injector = injectors.injector(injectableClass);
             injector.inject(object);    //Use double dispatch to avoid this dodgy code? or is DD more of problem, gets everywhere then
         }
+    }
+
+    private Class<?>[] findInjectableClasses(Object object) {
+        return object.getClass().getInterfaces();
     }
 
 }
