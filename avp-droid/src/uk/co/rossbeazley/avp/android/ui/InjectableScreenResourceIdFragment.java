cd8154fbb8ba3b064b95d778feb469ba59c05e91
rossbeazley <ross@rossbeazley.co.uk>
2014-03-11 13:47:34 +0000

    removed header doc

--- avp-droid/src/uk/co/rossbeazley/avp/android/ui/InjectableScreenResourceIdFragment.java
+++ avp-droid/src/uk/co/rossbeazley/avp/android/ui/InjectableScreenResourceIdFragment.java
@@ -1,12 +1,5 @@
 package uk.co.rossbeazley.avp.android.ui;
 
-/**
- * Created with IntelliJ IDEA.
- * User: beazlr02
- * Date: 30/01/2014
- * Time: 20:35
- * To change this template use File | Settings | File Templates.
- */
 public interface InjectableScreenResourceIdFragment {
     void injectFragmentScreenFactory(FragmentScreenFactory fragmentScreenFactory);
     void injectInflatedViewFactory(InflatedViewFactory inflatedViewFactory);
