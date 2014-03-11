6b7f32e15f6d0f0aa0c24413e772059dee59569a
rossbeazley <ross@rossbeazley.co.uk>
2014-03-11 17:16:06 +0000

    removed doc header

--- avp-droid/src/uk/co/rossbeazley/avp/android/ui/results/InjectableResultsFragmentInjector.java
+++ avp-droid/src/uk/co/rossbeazley/avp/android/ui/results/InjectableResultsFragmentInjector.java
@@ -2,13 +2,6 @@ package uk.co.rossbeazley.avp.android.ui.results;
 
 import uk.co.rossbeazley.avp.android.application.DependencyInjectors;
 
-/**
-* Created with IntelliJ IDEA.
-* User: beazlr02
-* Date: 11/03/2014
-* Time: 17:06
-* To change this template use File | Settings | File Templates.
-*/
 public class InjectableResultsFragmentInjector implements DependencyInjectors.Injector<InjectableResultsFragment> {
     @Override
     public void inject(InjectableResultsFragment object) {
