0fa79cc107bc835dc6a7e4c5b0bfacb283c0aab7
rossbeazley <ross@rossbeazley.co.uk>
2014-03-26 22:26:41 +0000

    MediaRepositry stub

--- avp-droid/src/uk/co/rossbeazley/avp/android/ui/search/SearchFragmentScreenFactory.java
+++ avp-droid/src/uk/co/rossbeazley/avp/android/ui/search/SearchFragmentScreenFactory.java
@@ -2,12 +2,18 @@ package uk.co.rossbeazley.avp.android.ui.search;
 
 import uk.co.rossbeazley.avp.android.media.MediaRepository;
 import uk.co.rossbeazley.avp.android.media.MediaRepositoryStub;
+import uk.co.rossbeazley.avp.android.media.Programme;
 import uk.co.rossbeazley.avp.android.search.CanDispatchSearchQuery;
+import uk.co.rossbeazley.avp.android.search.Query;
+import uk.co.rossbeazley.avp.android.search.Results;
 import uk.co.rossbeazley.avp.android.search.SearchService;
 import uk.co.rossbeazley.avp.android.ui.CanFindViewById;
 import uk.co.rossbeazley.avp.android.ui.FragmentScreenFactory;
 import uk.co.rossbeazley.avp.android.ui.Screen;
 import uk.co.rossbeazley.avp.eventbus.EventBus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SearchFragmentScreenFactory implements FragmentScreenFactory {
     private final EventBus bus;
@@ -24,6 +30,10 @@ public class SearchFragmentScreenFactory implements FragmentScreenFactory {
     }
 
     private void createPresenter(SearchScreenAndroid result) {
+        Map<Query, Results> resultsByQuery;
+        resultsByQuery = new HashMap<Query, Results>(1){{
+            put(Query.fromString("search"), new Results(new Programme()));
+        }};
         MediaRepository mediaRepository = new MediaRepositoryStub(bus, resultsByQuery);
         CanDispatchSearchQuery canDispatchSearchQuery = new SearchService(bus, mediaRepository); //TODO just newing the edge of the app here, dosnt feel quite right
         new SearchScreenPresenter(result, canDispatchSearchQuery);
