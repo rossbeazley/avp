ebb4d00d7bceb62a1d7b916395848031b09dcf83
rossbeazley <ross@rossbeazley.co.uk>
2014-04-01 13:07:05 +0100

    moved result list adapter to top level to start testing

---
+++ avp-droid/src/uk/co/rossbeazley/avp/android/ui/results/ResultsListAdapter.java
@@ -0,0 +1,74 @@
+package uk.co.rossbeazley.avp.android.ui.results;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import uk.co.rossbeazley.avp.android.search.Results;
+
+class ResultsListAdapter implements ListAdapter {
+
+    private final Results results;
+
+    public ResultsListAdapter(Results results) {
+        this.results = results;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int i) {
+        return true;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
--- avp-droid/src/uk/co/rossbeazley/avp/android/ui/results/ResultsScreenAndroid.java
+++ avp-droid/src/uk/co/rossbeazley/avp/android/ui/results/ResultsScreenAndroid.java
@@ -1,9 +1,6 @@
 package uk.co.rossbeazley.avp.android.ui.results;
 
-import android.database.DataSetObserver;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import uk.co.rossbeazley.avp.android.R;
 import uk.co.rossbeazley.avp.android.search.Results;
@@ -26,7 +23,7 @@ class ResultsScreenAndroid implements ResultsScreen {
     @Override
     public void showResults(Results results) {
         //TODO implement the show results method
-        findListView(R.id.searchresultslist).setAdapter(new ResultsListAdapter());
+        findListView(R.id.searchresultslist).setAdapter(new ResultsListAdapter(results));
     }
 
     private ListView findListView(int id) {
@@ -43,63 +40,4 @@ class ResultsScreenAndroid implements ResultsScreen {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    private static class ResultsListAdapter implements ListAdapter {
-        @Override
-        public boolean areAllItemsEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled(int i) {
-            return true;
-        }
-
-        @Override
-        public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-        }
-
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-        }
-
-        @Override
-        public int getCount() {
-            return 0;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
-        }
-
-        @Override
-        public int getItemViewType(int i) {
-            return 0;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 1;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-    }
 }
