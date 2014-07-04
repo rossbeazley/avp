aa46f6c77d0450cd1695f2b4254d7189c9456ee4
rossbeazley <ross@rossbeazley.co.uk>
2014-07-04 15:51:34 +0100

    changed spec of PRODUCER function --- now broken!!

--- event-bus/src/uk/co/rossbeazley/avp/eventbus/executor/ExecutorEventBus.java
+++ event-bus/src/uk/co/rossbeazley/avp/eventbus/executor/ExecutorEventBus.java
@@ -5,12 +5,15 @@ import uk.co.rossbeazley.avp.eventbus.EventBus;
 import uk.co.rossbeazley.avp.eventbus.EventSubscription;
 import uk.co.rossbeazley.avp.eventbus.singlethreaded.PayloadFunction;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 public class ExecutorEventBus implements EventBus {
 
     private final CanBuildExecutor canBuildExecutor;
     private EventSubscriptions eventSubscriptions = new EventSubscriptions();
+    private Map<Object, PayloadFunction> producers = new HashMap<Object, PayloadFunction>();
 
     public ExecutorEventBus() {
         canBuildExecutor = CanBuildExecutor.DEFAULT;
@@ -33,14 +36,16 @@ public class ExecutorEventBus implements EventBus {
 
     @Override
     public void registerProducer(Object event, PayloadFunction function) {
-
+        this.producers.put(event,function);
     }
 
 
     @Override
     public EventSubscription whenEvent(Object event) {
         Executor executor = canBuildExecutor.executor();
-        return eventSubscriptions.addSubscriberForEvent(event, new ExecutorEventSubscription(executor));
+        ExecutorEventSubscription subscription = new ExecutorEventSubscription(executor);
+
+        return eventSubscriptions.addSubscriberForEvent(event, subscription);
     }
 
 
